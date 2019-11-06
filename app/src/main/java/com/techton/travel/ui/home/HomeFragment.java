cfa3790d4372b62fc4285445ed53f7e6acbe2ba6
sechiyo97 <sechiyo97@gmail.com>
2019-11-06 23:00:46 +0900

    nav 프래그먼트에 지도 추가, main 조금 정리

--- app/src/main/java/com/techton/travel/ui/home/HomeFragment.java
+++ app/src/main/java/com/techton/travel/ui/home/HomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -23,6 +24,14 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        ImageButton searchButton = getActivity().findViewById(R.id.home_search);
+        /*searchButton.setOnClickListener(new ImageButton.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+            }
+        });*/
 
         return root;
     }
